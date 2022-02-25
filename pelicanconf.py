639a3731ca8485fa4fb106e802bea002a73eaee7
figueroaric <62219595+figueroaric@users.noreply.github.com>
2022-02-25 17:33:54 -0500

    Update Master with Themes

--- pelicanconf.py
+++ pelicanconf.py
@@ -6,7 +6,7 @@ PATH = 'content'
 
 TIMEZONE = 'America/New_York'
 
-DEFAULT_LANG = 'English'
+DEFAULT_LANG = 'en'
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
@@ -26,6 +26,19 @@ SOCIAL = (('You can add links in your config file', '#'),
           ('Another social link', '#'),)
 
 DEFAULT_PAGINATION = 5
+
+#pelicanconf.py
+
+PLUGIN_PATHS = ['pelican-plugins']
+
+THEME = 'pelican-themes/pelican-bootstrap3'
+BOOTSTRAP_THEME = 'flatly'
+
+#PLUGIN_PATHS = ['/path/to/git/pelican-plugins']
+JINJA_ENVIRONMENT = {'extensions': ['jinja2.ext.i18n']}
+PLUGINS = ['i18n_subsites']
+
+I18N_TEMPLATES_LANG = 'en'
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
